97053f3297995553587f15da2303681ec343adb8
Joh Johannsen <jojolosangeles2017@gmail.com>
2019-12-02 06:54:53 -0800

    fix json

--- es_search.py
+++ es_search.py
@@ -1,21 +1,16 @@
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, connections
-from botocore.vendored import requests
+import os
 
-def search(s):
-    response = requests.get("http://885d447e.ngrok.io/")
-    return response.json()
-    # prepare to search
-    # client = Elasticsearch(
-    #     ['885d447e.ngrok.io'],#:443'],
-    #     # turn on SSL
-    #     #use_ssl=True
-    # )
-    # searcher = Search(index='human_index')[0:2].using(client).query("match", raw_data=s)
-    # searcher.source(excludes=["raw_data"])
-    # sresponse = searcher.execute()
-    # print(f"search for  {s}")
-    # response = []
-    # for hit in searcher:
-    #     response.append({ 'species': hit.species, 'chromosome': hit.chromosome, 'score': hit.meta.score })
-    # return response
+def es_search(s):
+    #prepare to search
+    connections.create_connection(hosts=[os.environ['ELASTICSEARCH_HOST']], timeout=20)
+    client = Elasticsearch()
+    searcher = Search(index='human_index')[0:2].using(client).query("match", raw_data=s)
+    searcher.source(excludes=["raw_data"])
+    sresponse = searcher.execute()
+    print(f"search for  {s}")
+    response = []
+    for hit in searcher:
+        response.append({ 'species': hit.species, 'chromosome': hit.chromosome, 'score': hit.meta.score })
+    return response
--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,12 @@
 import json
 from search import search
+from es_search import es_search
     
 def lambda_handler(event, context):
     seq = event['seq']
     result = search(seq)
     return {
         'statusCode': 200,
-        'body': json.dumps(result)
+        'body': result
     }
 
