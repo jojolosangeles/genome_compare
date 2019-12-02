cd6c78b8cc1ff56bf9bf59413736897b8c5fdce0
Joh Johannsen <jojolosangeles2017@gmail.com>
2019-12-01 20:23:57 -0800

    not working

--- es_search.py
+++ es_search.py
@@ -1,18 +1,21 @@
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, connections
+from botocore.vendored import requests
 
 def search(s):
-    # set up connections
-    connections.create_connection(alias='local_connection', hosts=['localhost'], timeout=60)
-    connections.create_connection(alias='remote_connection', hosts=['885d447e.ngrok.io'], timeout=60)
-    connectionName = 'remote_connection'
-
+    response = requests.get("http://885d447e.ngrok.io/")
+    return response.json()
     # prepare to search
-    client = Elasticsearch()
-    searcher = Search(using=connectionName, index='human_index')[0:2].using(client).query("match", raw_data=s)
-    sresponse = searcher.execute()
-    print(f"search for  {s}")
-    response = []
-    for hit in searcher:
-        response.append({ 'species': hit.species, 'chromosome': hit.chromosome, 'score': hit.meta.score })
-    return response
+    # client = Elasticsearch(
+    #     ['885d447e.ngrok.io'],#:443'],
+    #     # turn on SSL
+    #     #use_ssl=True
+    # )
+    # searcher = Search(index='human_index')[0:2].using(client).query("match", raw_data=s)
+    # searcher.source(excludes=["raw_data"])
+    # sresponse = searcher.execute()
+    # print(f"search for  {s}")
+    # response = []
+    # for hit in searcher:
+    #     response.append({ 'species': hit.species, 'chromosome': hit.chromosome, 'score': hit.meta.score })
+    # return response
--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,22 @@
 import json
 from es_search import search
+from botocore.vendored import requests
 
 def lambda_handler(event, context):
     MIN_SEQ_LEN = 10
-    seq = event['path'].split("/")[-1]
-    print(event['path'])
-    n = len(seq)
-    print(f"n is {n}")
-    if n < MIN_SEQ_LEN:
-        message = f"search sequence must have at least {MIN_SEQ_LEN} values, this one has {n} values"
-    else:
-        result = search(seq)
-        message = seq
-    result = {
-        "message": message
-    }
+    print(json.dumps(event))
+    result = requests.get("http://885d447e.ngrok.io/")
+    # seq = event['seq']
+    # n = len(seq)
+    # print(f"n is {n}")
+    # if n < MIN_SEQ_LEN:
+    #     message = f"search sequence must have at least {MIN_SEQ_LEN} values, this one has {n} values"
+    # else:
+    #     result = search(seq)
+    #     message = seq
+    # result = {
+    #     "message": message
+    # }
     return {
         'statusCode': 200,
         'headers': {
